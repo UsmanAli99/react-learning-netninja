66bea0857b91677d6fc1e0f5e3ec8d68cf6c8acc
UsmanAli <usmanali.3099@gmail.com>
2022-10-31 23:06:06 +0500

    Practice dynamic values in component

--- dojo-app/src/App.js
+++ dojo-app/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const comments = 50;
+  // const objects = {'name': 'Usman Ali', 'age': 52};
+  const isDeactivate = true;
+  const href = "https://mail.google.com/mail/u/0/"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Total Comments: { comments } </p>
+        {/* <p>{ objects }</p> */}
+        <p>{ 10 }</p>
+        <p>{ Math.random() }</p>
+        <a href={href}>Click here</a>
       </div>
     </div>
   );
