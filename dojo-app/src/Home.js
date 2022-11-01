137beabbb134bf29c80cb720ccabf91ef39600e0
UsmanAli <usmanali.3099@gmail.com>
2022-11-01 16:58:14 +0500

    Outputting List

--- dojo-app/src/Home.js
+++ dojo-app/src/Home.js
@@ -1,20 +1,21 @@
 import { useState } from 'react'
 
 const Home = () => {
-  // let name = "Usman Ali"
-  const [name, setName] = useState('Usman Ali');
-  const [age, setAge] = useState(23);
-
-  const handleClick = () => {
-    setName('Ali Usman');
-    setAge(24);
-  }
+  const [blogs, setBlog] = useState([
+    {title: "How to learn", body: "This is lrom", author: "Usman Ali", "id": 1},
+    {title: "How to glearn", body: "This is lrom", author: "Umer Abdullah", "id": 2},
+    {title: "How to clearn", body: "This is lrom", author: "Ghilman Ahmed", "id": 3},
+  ])
 
   return (
     <div className="home">
       <h1>HomePage</h1>
-      <p>{ name } age is {age}</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>{ blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 };
