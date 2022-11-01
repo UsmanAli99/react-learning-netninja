0304a78f912dc5e743bc3baf2a3e17f917199ca2
UsmanAli <usmanali.3099@gmail.com>
2022-11-01 13:19:58 +0500

    Handle events

--- dojo-app/src/Home.js
+++ dojo-app/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('Hello World', e);
+  }
+
+  const handleAgainClick = (name, e) => {
+    console.log('Hello ' + name, e);
+  }
+
   return (
     <div className="home">
       <h1>HomePage</h1>
+      <button onClick={ handleClick }>Click me</button>
+      <button onClick={(e) => handleAgainClick('Usman Ali', e)}>Click me Again</button>
     </div>
   );
 };
