b067a4b11b8e5a2ac1b3478b70353e77ce84dd5c
UsmanAli <usmanali.3099@gmail.com>
2022-11-01 16:23:03 +0500

    Learn react-hooks and useState

--- dojo-app/src/Home.js
+++ dojo-app/src/Home.js
@@ -1,17 +1,20 @@
-const Home = () => {
-  const handleClick = (e) => {
-    console.log('Hello World', e);
-  }
+import { useState } from 'react'
 
-  const handleAgainClick = (name, e) => {
-    console.log('Hello ' + name, e);
+const Home = () => {
+  // let name = "Usman Ali"
+  const [name, setName] = useState('Usman Ali');
+  const [age, setAge] = useState(23);
+
+  const handleClick = () => {
+    setName('Ali Usman');
+    setAge(24);
   }
 
   return (
     <div className="home">
       <h1>HomePage</h1>
-      <button onClick={ handleClick }>Click me</button>
-      <button onClick={(e) => handleAgainClick('Usman Ali', e)}>Click me Again</button>
+      <p>{ name } age is {age}</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
