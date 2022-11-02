00ce465a4347d4d2b15c3bdebfb9bbde4c9a4e04
UsmanAli <usmanali.3099@gmail.com>
2022-11-02 10:57:28 +0500

    Reusing components

--- dojo-app/src/Home.js
+++ dojo-app/src/Home.js
@@ -5,13 +5,14 @@ const Home = () => {
   const [blogs, setBlog] = useState([
     {title: "How to learn", body: "This is lrom", author: "Usman Ali", "id": 1},
     {title: "How to glearn", body: "This is lrom", author: "Umer Abdullah", "id": 2},
-    {title: "How to clearn", body: "This is lrom", author: "Ghilman Ahmed", "id": 3},
+    {title: "How to clearn", body: "This is lrom", author: "Usman Ali", "id": 3},
   ])
 
   return (
     <div className="home">
       <h1>HomePage</h1>
       <BlockList blogs={blogs} title="All blogs" />
+      <BlockList blogs={blogs.filter((blog) => blog.author === 'Usman Ali')} title="Secondly all blogs"/>
     </div>
   );
 };
