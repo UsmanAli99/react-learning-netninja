afdd2c9a14cc2f708e7f4bdcec44d6cabd3fa4a6
UsmanAli <usmanali.3099@gmail.com>
2022-11-03 12:50:13 +0500

    Fetch data from api and remove delete blog

--- dojo-app/src/Home.js
+++ dojo-app/src/Home.js
@@ -2,26 +2,23 @@ import { useState, useEffect } from 'react'
 import BlockList from './BlogList'
 
 const Home = () => {
-  const [blogs, setBlog] = useState([
-    {title: "How to learn", body: "This is lrom", author: "Usman Ali", "id": 1},
-    {title: "How to glearn", body: "This is lrom", author: "Umer Abdullah", "id": 2},
-    {title: "How to clearn", body: "This is lrom", author: "Usman Ali", "id": 3},
-  ])
+  const [blogs, setBlog] = useState(null)
   const [name, setName] = useState('Usman')
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlog(newBlogs);
-  }
-
   useEffect(() => {
-    console.log('Yes you are right');
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlog(data);
+      })
+  }, []);
 
   return (
     <div className="home">
       <h1>HomePage</h1>
-      <BlockList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+      {blogs && <BlockList blogs={blogs} title="All blogs"/>}
       <button onClick={() => {setName('Ali')}}>Click here</button>
       <p>{name}</p>
     </div>
